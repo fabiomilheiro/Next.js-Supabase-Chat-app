50d9dff10b5c1c6ae964951383794f647ccb726b
Fabio Milheiro <Fabio.Milheiro@redington.co.uk>
2022-02-25 22:26:08 +0000

    Create next.js app with useSupabase hook

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import useSupabase from "../utils/useSupabase";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { session, supabase } = useSupabase();
+
+  return <Component session={session} supabase={supabase} {...pageProps} />;
+}
+
+export default MyApp;
